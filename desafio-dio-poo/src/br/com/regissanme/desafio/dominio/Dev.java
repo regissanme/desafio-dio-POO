72b82e554bb0d102268211ee022a4d9ab1350833
regissanme <rsanme@gmail.com>
2022-08-04 11:09:53 -0300

    create Dev and Bootcamp classes

---
+++ desafio-dio-poo/src/br/com/regissanme/desafio/dominio/Dev.java
@@ -0,0 +1,78 @@
+package br.com.regissanme.desafio.dominio;
+
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+
+/**
+ * Projeto: desafio-dio-poo
+ * Desenvolvedor: Reginaldo Santos de Medeiros (regissanme)
+ * Data: 04/08/2022
+ * Hora: 10:42
+ */
+public class Dev {
+
+    private String nome;
+    private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
+    private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Conteudo> getConteudosInscritos() {
+        return conteudosInscritos;
+    }
+
+    public void setConteudosInscritos(Set<Conteudo> conteudosInscritos) {
+        this.conteudosInscritos = conteudosInscritos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dev dev = (Dev) o;
+        return Objects.equals(nome, dev.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome);
+    }
+
+    public Set<Conteudo> getConteudosConcluidos() {
+        return conteudosConcluidos;
+    }
+
+    public void setConteudosConcluidos(Set<Conteudo> conteudosConcluidos) {
+        this.conteudosConcluidos = conteudosConcluidos;
+    }
+
+    public void inscreverBootcamp(Bootcamp bootcamp){
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());
+        bootcamp.getDevsIncritos().add(this);
+    }
+
+    public void progredir(){
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+        if (conteudo.isPresent()){
+            this.conteudosConcluidos.add(conteudo.get());
+            this.conteudosInscritos.remove(conteudo.get());
+        } else {
+            System.err.println("Você não está matriculado em nenhum Conteúdo!");
+        }
+    }
+
+    public double calcularTotalXp(){
+       return this.getConteudosConcluidos()
+               .stream()
+               .mapToDouble(Conteudo::calcularXp)
+               .sum();
+    }
+}
